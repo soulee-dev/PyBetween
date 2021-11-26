e73fe4ef8f88ff049641355cc4db9d5b9de52bc6
lill74 <a0103932@naver.com>
2021-11-26 21:55:32 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='PyBetween',
+    version='1.0.2',
+    license='MIT',
+    author='lill74',
+    author_email='a010393223@gmail.com',
+    description='Python wrapper for Between app',
+    long_description_content_type="text/markdown",
+    long_description=open('README.md', encoding='UTF8').read(),
+    url='https://github.com/lill74/PyBetween',
+    packages = setuptools.find_packages(),
+)
